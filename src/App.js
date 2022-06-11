d399376170cc17a6e742e3260bc15403edf93974
sadock-cmd <sadockpiason@gmail.com>
2022-06-11 10:08:04 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World, lets do this my Gees. ONE LOVE!
+        Hello World, lets do this my Gees. ONE LOVE!<br/><hr/>
+        Working on fyp....
       </header>
     </div>
   );
